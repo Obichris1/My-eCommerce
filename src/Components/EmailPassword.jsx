b53716a179d4b94d7683012a3e0c66805dbe4b36
Obichris1 <obichris202@gmail.com>
2023-08-17 20:57:33 +0100

    Password reset

--- src/Components/EmailPassword.jsx
+++ src/Components/EmailPassword.jsx
@@ -3,9 +3,15 @@ import AuthWrapper from "./AuthWrapper";
 import Input from "./Form/Input";
 import Button from "./Form/Button";
 import { useState } from "react";
+import { auth } from "../Firebase/utils";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { Link } from "react-router-dom";
+import { withRouter } from "./withRouter";
 
 const EmailPassword = () => {
   const [email, setEmail] = useState("");
+
+  const [errors, setErrors] = useState("");
 
   const emailHandler = (e) => {
     setEmail(e.target.value);
@@ -14,10 +20,34 @@ const EmailPassword = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
+
+    try {
+      const config = {
+        url: "http://localhost:3000/login",
+      };
+      await sendPasswordResetEmail(auth, email, config)
+        .then(() => {
+          <Link to="/login"></Link>;
+        })
+
+        .catch(() => {
+          const err = ["Email not found"];
+          setErrors(err);
+        });
+    } catch (error) {
+      // console.log(error);
+    }
   };
 
   return (
     <AuthWrapper>
+      {errors.length > 0 && (
+        <ul>
+          {errors.map((e, index) => {
+            <li key={index}>{e}</li>;
+          })}
+        </ul>
+      )}
       <form onSubmit={submitHandler}>
         <Input
           type="email"
